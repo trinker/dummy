bf64675f1949998f11c77af562f5c3fbc6262884
tyler rinker <tyler_rinker@hotmail.com>
2013-05-31 12:55:06 -0400

    updated dummy

--- R/dummy-package.R
+++ R/dummy-package.R
@@ -1,22 +1,9 @@
-#' qdap: Quantitative Discourse Analysis Package
+#' Dummy
 #'
-#' This package automates many of the tasks associated with quantitative 
-#' discourse analysis oftranscripts containing discourse including frequency 
-#' counts of sentence types, words, sentence, turns of talk, syllable counts and 
-#' other assorted analyysis tasks.  The package provides parsing tools for 
-#' preparing transcript data.  Many functions enable the user to aggregate data 
-#' by any number of grouping variables providing analysis and seamless 
-#' integration with other R packages that undertake higher level analysis and 
-#' visualization of text.  This provides the user with a more efficient and 
-#' targeted analysis.
+#' Dumbness
 #'
 #' @docType package
-#' @name qdap
-#' @Imports: gridExtra, chron, scales, RColorBrewer, igraph, tm, wordcloud, 
-#' venneuler, openNLPmodels.en, Snowball, gplots, gridExtra, gdata, 
-#' openNLP, XML, RCurl, parallel
-#' @Suggests: koRpus
-#' @LazyData: TRUE
-#' @aliases qdap package-qdap
+#' @name dummy
+#' @aliases summy package-dummy
 NULL
 
---
+++ R/test.R
@@ -0,0 +1,16 @@
+#' Test
+#' 
+#' Test 2
+#' 
+#' @param x An object
+#' @section Suggestion: Looky here: 
+#' \enumerate{ 
+#'   \item{\bold{Item 1} - Foo}
+#'   \item{\bold{Item 2} - Bar:  
+#'   \href{https://github.com/hakimel/reveal.js/\#installation}{click me}}   
+#' }
+#' @export 
+test <- function(x = "test") {
+    print(x)
+}
+
